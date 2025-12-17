c97832fb62c0f32c001e257ba5cb972ea8ba05e4
Tim Ross <timrross@gmail.com>
2025-12-17 16:56:55 +0000

    fix a path to the database

--- src/integrations/uciDataride/syncRidersFromRankings.ts
+++ src/integrations/uciDataride/syncRidersFromRankings.ts
@@ -1,4 +1,4 @@
-import { db } from "../../server/db";
+import { db } from "../../../server/db";
 import { riders, type InsertRider, type Rider } from "@shared/schema";
 import { eq } from "drizzle-orm";
 
